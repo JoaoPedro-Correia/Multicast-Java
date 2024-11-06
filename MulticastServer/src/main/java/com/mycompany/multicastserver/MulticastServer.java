fb99e659811b88596387360be5bbc3f4dae4d83a
JoaoPedro-Correia <joaopcorreia014@gmail.com>
2024-11-06 16:45:22 -0300

    upd: files

---
+++ MulticastServer/src/main/java/com/mycompany/multicastserver/MulticastServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multicastserver;
+
+/**
+ *
+ * @author correia
+ */
+public class MulticastServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
